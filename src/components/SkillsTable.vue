5ac35c4c184c71ffa6a31c1c417e881d8d46b050
steve rigby <steve.rigby@rewardstyle.com>
2024-12-29 22:57:09 -0500

    feat: added store. hydrates modifiers based on aspects, wounds, inventory

--- src/components/SkillsTable.vue
+++ src/components/SkillsTable.vue
@@ -13,19 +13,39 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="skill in skills" :key="skill.name">
+        <tr v-for="skill in modifiers" :key="skill.name">
           <td>{{ skill.name }}</td>
-          <td><button>{{ skill.attack }}</button></td>
-          <td><button>{{ skill.defend }}</button></td>
-          <td><button>{{ skill.overcome }}</button></td>
-          <td><button>{{ skill.empower }}</button></td>
+          <td><button>{{ skill.attack < 0 ? skill.attack : `+${skill.attack}` }}</button></td>
+          <td><button>{{ skill.defend < 0 ? skill.defend : `+${skill.defend}` }}</button></td>
+          <td><button>{{ skill.overcome < 0 ? skill.overcome : `+${skill.overcome}` }}</button></td>
+          <td><button>{{ skill.empower < 0 ? skill.empower : `+${skill.empower}` }}</button></td>
         </tr>
       </tbody>
     </table>
   </div>
 </template>
-
 <script>
+import { useCharacterStore } from '@/stores/characterStore';
+
+export default {
+  setup() {
+    const characterStore = useCharacterStore();
+    characterStore.computeModifiers(characterStore);
+
+    // Example of accessing state and actions
+    const addItem = () => {
+      characterStore.addToInventory({ name: 'Health Potion', quantity: 1 });
+    };
+
+    return {
+      modifiers: characterStore.modifiers,
+      characterName: characterStore.characterName,
+      addItem,
+    };
+  },
+};
+</script>
+<!-- <script>
 export default {
   data() {
     return {
@@ -38,7 +58,7 @@ export default {
     };
   },
 };
-</script>
+</script> -->
 
 <style scoped>
 .skills {
@@ -70,3 +90,4 @@ button:hover {
   background-color: #0056b3;
 }
 </style>
+
--- src/stores/characterStore.js
+++ src/stores/characterStore.js
@@ -3,11 +3,93 @@ import { defineStore } from 'pinia'
 export const useCharacterStore = defineStore('character', {
   // State is where you define your data
   state: () => ({
-    character: {
-      name: '',
-      skills: [],
-      inventory: [],
+    name: 'mokey d. luffy',
+    epithet: 'the man who will become king of the pirates',
+    skills: {
+      agility: 4,
+      brawl: 3,
+      contacts: 3,
+      insight: 2,
+      operate: 2,
+      perform: 2,
+      persuade: 1,
+      physique: 1,
+      stealth: 1,
+      shoot: 1,
     },
+    channels: [{}],
+    specials: [{}],
+    aspects: [
+      {
+        name: 'rubber arms',
+        description: 'strecthy boy is stretchy',
+        effects: [
+          {
+            skills: ['agility'],
+            actions: ['attack', 'defend'],
+            modifier: 1,
+            channel: 'elemental',
+            description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          },
+          {
+            skills: ['brawl'],
+            actions: ['attack'],
+            modifier: 2,
+            channel: 'organic',
+            description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          },
+        ],
+      },
+      {
+        name: 'walking concussion',
+        description: 'meat brain',
+        effects: [
+          {
+            skills: ['stealth', 'insight'],
+            actions: ['empower'],
+            modifier: -1,
+            channel: '',
+            description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          },
+          {
+            skills: ['contacts'],
+            actions: ['empower'],
+            modifier: 1,
+            channel: 'tech',
+            description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          },
+        ],
+      },
+    ],
+    wounds: [
+      {
+        name: 'broken leg',
+        description: 'leg broked',
+        effects: [
+          {
+            skills: ['perform', 'insight'],
+            actions: ['empower'],
+            modifier: -5,
+            channel: '',
+          },
+        ],
+      },
+    ],
+    inventory: [
+      {
+        name: 'straw hat',
+        description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        effects: [
+          {
+            skills: ['persuade', 'operate'],
+            actions: ['overcome'],
+            modifier: 5,
+            channel: '',
+          },
+        ],
+      },
+    ],
+    modifiers: {},
   }),
 
   // Actions are for modifying state
@@ -16,21 +98,53 @@ export const useCharacterStore = defineStore('character', {
       this.character = data
     },
     addToInventory(item) {
-      this.character.inventory.push(item)
+      this.inventory.push(item)
     },
-    updateSkill(skill) {
-      const index = this.character.skills.findIndex((s) => s.name === skill.name)
-      if (index !== -1) {
-        this.character.skills[index] = skill
-      } else {
-        this.character.skills.push(skill)
+    updateSkill(skill, val) {
+      this.skills[skill] = val
+    },
+    computeModifiers: (store) => {
+      const { skills, aspects, wounds, inventory } = store
+      const modifiers = {}
+
+      // init modifiers
+      const actions = ['attack', 'defend', 'overcome', 'empower']
+      for (const skill in skills) {
+        modifiers[skill] = { name: skill, sources: [] }
+        actions.forEach((action) => {
+          modifiers[skill][action] = skills[skill]
+        })
       }
+
+      const applyModifiers = (source, sourceName) => {
+        source.forEach((item) => {
+          item.effects.forEach((effect) => {
+            effect.skills.forEach((skill) => {
+              effect.actions.forEach((action) => {
+                if (modifiers[skill] && modifiers[skill][action] !== undefined) {
+                  modifiers[skill].sources.push(item)
+                  modifiers[skill][action] += effect.modifier
+                } else {
+                  console.warn(`skipped ${skill} ${action} from ${sourceName} ${item.name}`)
+                }
+              })
+            })
+          })
+        })
+      }
+      // Step 2: Apply effects from aspects, wounds, and inventory
+      applyModifiers(aspects, 'aspect')
+      applyModifiers(wounds, 'wound')
+      applyModifiers(inventory, 'inventory')
+
+      // Update the store's modifiers
+      store.modifiers = { ...modifiers }
     },
   },
 
   // Getters are computed properties for state
   getters: {
-    inventoryCount: (state) => state.character.inventory.length,
-    characterName: (state) => state.character.name,
+    inventoryCount: (state) => state.inventory.length,
+    characterName: (state) => state.name,
   },
 })
