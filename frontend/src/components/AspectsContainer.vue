feb40cc29d459bb05031b056d78ca9520bf0bda4
steve rigby <steve.rigby@rewardstyle.com>
2025-02-05 20:05:25 -0500

    beginning aspects work

--- frontend/src/components/AspectsContainer.vue
+++ frontend/src/components/AspectsContainer.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="grid grid-cols-3 gap-4 p-4">
-    <div v-for="(aspects, index) in columns" :key="index" class="flex flex-col gap-2">
-      <button v-for="btn in aspects" :key="btn" class="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700">
-        Button {{ btn }}
-      </button>
-    </div>
+    <button v-for="btn in aspects" :key="btn" class="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700">
+      {{ btn.name }}
+    </button>
   </div>
   <div class="flex justify-center mt-4">
     <button @click="generateColumns" class="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-700">
@@ -13,8 +11,20 @@
   </div>
 </template>
 
-<script setup>
+<script>
 import { ref, onMounted } from 'vue';
+import { useCharacterStore } from '@/stores/characterStore'
+
+export default {
+  setup() {
+    const aspects = useCharacterStore().aspects
+    return {
+      aspects
+    }
+
+  }
+}
+
 
 const columns = ref([[], [], []]);
 
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -69,7 +69,7 @@ export default {
   height: 95vh;
   padding: 1rem;
   grid-template-columns: 1fr 3fr;
-  grid-template-rows: 20% 20% 60%;
+  grid-template-rows: 30% 10% 60%;
   grid-column-gap: 12px;
   grid-row-gap: 12px;
 }
